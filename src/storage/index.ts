d29977107ee60f642104d207d11ac24296422f5b
Kittiphat S <freeuxer@gmail.com>
2021-05-13 04:06:18 +0700

    fixed yarn built

--- src/storage/index.ts
+++
@@ -1,7 +0,0 @@
-export interface CacheStorage {
-  canCachePromise(): boolean
-  get(key: string): Promise<any | undefined> | any | undefined
-  set(key: string, value: any, exp: number): void
-}
-
-export * from './inmemory'
