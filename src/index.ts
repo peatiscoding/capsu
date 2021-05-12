2f976caffc7e28830b39feab1b550374dc87fed6
Kittiphat S <freeuxer@gmail.com>
2021-05-13 04:08:17 +0700

    merge files together

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-import type { CacheStorage } from './storage'
-import { InMemoryStorage } from './storage'
+export interface CacheStorage {
+  canCachePromise(): boolean
+  get(key: string): Promise<any | undefined> | any | undefined
+  set(key: string, value: any, exp: number): void
+}
+
+export class InMemoryStorage implements CacheStorage {
+
+  public store: { [key: string]: { value: Promise<any> | any, exp: number } } = {}
+
+  canCachePromise() {
+    return true
+  }
+
+  get(key: string): any | undefined {
+    const found = this.store[key]
+    if (!found || found.exp < new Date().getTime()) {
+      return undefined
+    }
+    return found.value
+  }
+
+  set(key: string, value: any | Promise<any>, exp: number) {
+    this.store[key] = { value, exp }
+  }
+}
+
 
 interface CacheKeyOptions {
   /**
